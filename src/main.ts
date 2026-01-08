c2e2d53092e0737e60ddafdc2a629e417fe02d81
heyManNice <3031978990@qq.com>
2026-01-08 13:00:49 +0800

    fix：修改默认值位置

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
-// @ts-ignore
-const configFile = (import.meta.glob('/hey-i18n.config.ts', { eager: true })['/hey-i18n.config.ts'] as { default: HeyI18nConfig })?.default ?? {};
-
 type HeyI18nConfig = {
     i18nFiles: Record<string, () => Promise<unknown>>;
     sourcesLocale: string;
     defaultLocale: 'system' | string;
 }
 
-export function defineConfig(config: Partial<HeyI18nConfig>): HeyI18nConfig {
-    return {
-        // @ts-ignore
-        i18nFiles: config.i18nFiles || import.meta.glob('/i18n/*.json') || {},
-        sourcesLocale: config.sourcesLocale || 'zh-CN',
-        defaultLocale: config.defaultLocale || 'system',
-    };
+// @ts-ignore
+const configFile = (import.meta.glob('/hey-i18n.config.ts', { eager: true })['/hey-i18n.config.ts'] as { default: HeyI18nConfig })?.default ?? {};
+
+export function defineConfig(config: Partial<HeyI18nConfig>) {
+    return config;
 }
 
-console.log(configFile);
+const config = {
+    // @ts-ignore
+    i18nFiles: configFile.i18nFiles || import.meta.glob('/i18n/*.json') || {},
+    sourcesLocale: configFile.sourcesLocale || 'zh-CN',
+    defaultLocale: configFile.defaultLocale || 'system',
+};
 
-const locales = Object.entries(configFile.i18nFiles).reduce((acc, [path, module]) => {
+const locales = Object.entries(config.i18nFiles).reduce((acc, [path, module]) => {
     const locale = path.split('/').pop()?.split('.').shift();
     if (locale) {
         acc[locale] = module;
@@ -32,7 +32,7 @@ function getSystemLocale(): string {
     return navigator.language || document.documentElement.lang || 'en-US';
 }
 
-let currentLocale = localStorage.getItem('hey-i18n-locale') || (configFile.defaultLocale === 'system' ? getSystemLocale() : configFile.defaultLocale);
+let currentLocale = localStorage.getItem('hey-i18n-locale') || (config.defaultLocale === 'system' ? getSystemLocale() : config.defaultLocale);
 
 console.log('当前语言：', currentLocale);
 
