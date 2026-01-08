4de6a65685b417dde8ef14576db40357fe995e44
heyManNice <3031978990@qq.com>
2026-01-08 17:55:57 +0800

    添加分栏和深色模式

--- studio/frontend/App.vue
+++ studio/frontend/App.vue
@@ -1,7 +1,14 @@
 <template>
-    <el-button type="primary">Hello Element Plus</el-button>
+    <el-splitter>
+        <el-splitter-panel size="250" :min="100">
+            <div class="demo-panel">1</div>
+        </el-splitter-panel>
+        <el-splitter-panel :min="200">
+            <div class="demo-panel">2</div>
+        </el-splitter-panel>
+    </el-splitter>
 </template>
 
 <script setup lang="ts">
-import { ElButton } from 'element-plus'
+import { ElSplitter, ElSplitterPanel } from 'element-plus';
 </script>
--- studio/frontend/main.ts
+++ studio/frontend/main.ts
@@ -1,6 +1,10 @@
 import App from './App.vue';
 import { createApp } from 'vue';
 
-import 'element-plus/dist/index.css'
+import { useDark } from '@vueuse/core';
+useDark();
+
+import 'element-plus/dist/index.css';
+import 'element-plus/theme-chalk/dark/css-vars.css';
 
 createApp(App).mount(document.body);
