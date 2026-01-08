7aa1645da01ffefbf0c946db3ac2183ae55b1366
heyManNice <3031978990@qq.com>
2026-01-08 14:47:35 +0800

    feat：支持拼接字符串

--- src/hey-i18n/translate.ts
+++ src/hey-i18n/translate.ts
@@ -1,6 +1,20 @@
 import { locale } from './locales';
 
-export default function translate(strings: TemplateStringsArray): string {
-    const key = strings[0] as string;
-    return locale[key] ?? key;
+export default function translate(strings: TemplateStringsArray, ...values: any[]): string {
+    const key = strings.join('{}');
+
+    const translated = locale[key];
+    if (translated) {
+        return translated;
+    }
+
+    // 匹配不到翻译的时候
+    let result = '';
+    for (let i = 0; i < strings.length; i++) {
+        result += strings[i];
+        if (i < values.length) {
+            result += values[i];
+        }
+    }
+    return result;
 }
