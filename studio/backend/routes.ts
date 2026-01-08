7e66dd09f8b24035bcd0f6bcb592e348a1015fae
heyManNice <3031978990@qq.com>
2026-01-09 02:54:58 +0800

    style

--- studio/backend/routes.ts
+++ studio/backend/routes.ts
@@ -3,9 +3,9 @@ import { IncomingMessage, ServerResponse } from 'http'
 type RouteHandler = (req: IncomingMessage, res: ServerResponse) => Promise<void>
 
 const routes: Record<string, RouteHandler> = {
-    "get:/": async (req, res) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
-        res.end('hello 你好，这是根路径')
+    "post:/functions": async (req, res) => {
+        res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' })
+        res.end(JSON.stringify({ message: "function executed successfully." }))
     }
 };
 
