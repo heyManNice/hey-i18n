8b94b40c7a88487eca04787e46afbd90a4a586af
heyManNice <3031978990@qq.com>
2026-01-16 16:36:33 +0800

    feat：添加命令行解析参数功能

--- studio/backend/main.ts
+++ studio/backend/main.ts
@@ -1,3 +1,28 @@
 #!/usr/bin/env node
+
 import Server from './server';
-const server = new Server(3034);
+import { Command } from 'commander';
+import packageJson from '../../package.json' assert { type: "json" };
+
+const program = new Command();
+
+program.name('hey-i18n-studio')
+    .version(packageJson.version);
+
+
+program.command('lint')
+    .description('Lint localization files')
+    .action(() => {
+        console.log('Linting localization files...');
+    });
+
+program.command('gui')
+    .description('Start the hey-i18n-studio GUI server')
+    .option('-p, --port <number>', 'Port to run the server on', '3034')
+    .option('-o, --open', 'Open the GUI in the default browser', false)
+    .option('-e, --expose', 'Expose the server to the local network', false)
+    .action((options) => {
+        const server = new Server(options.port);
+    });
+
+program.parse(process.argv);
