7b6ba56fc15e6c74d77cf32688ba36b128f335c9
heyManNice <3031978990@qq.com>
2026-01-19 01:14:37 +0800

    fix：修复this指向丢失的问题

--- studio/backend/rpc/rpc.ts
+++ studio/backend/rpc/rpc.ts
@@ -9,14 +9,22 @@ export default async function rpc(req: IncomingMessage, res: ServerResponse) {
     req.on('end', async () => {
         try {
             const { funcs, args } = JSON.parse(body);
-            let func: any = backend;
-            for (const f of funcs) {
-                func = func[f];
-                if (func === undefined) {
+            let target: any = backend;
+
+            for (const f of funcs.slice(0, -1)) {
+                target = target[f];
+                if (target === undefined) {
                     throw new Error(`${funcs.join('.')} is not defined`);
                 }
             }
-            const result = await func(...args);
+
+            const methodName = funcs[funcs.length - 1];
+            const method = target[methodName];
+            if (typeof method !== "function") {
+                throw new Error(`${funcs.join('.')} is not a function`);
+            }
+
+            const result = await method.apply(target, args);
             res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
             res.end(JSON.stringify({ result }));
         } catch (error: any) {
