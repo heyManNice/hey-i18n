3abaaa01dee97bd2206f6d32f39c009d5aa0ef12
heyManNice <3031978990@qq.com>
2026-02-17 20:21:17 +0800

    style

--- studio/frontend/views/Editor/TranslationCompare/EditableCellRenderer.vue
+++ studio/frontend/views/Editor/TranslationCompare/EditableCellRenderer.vue
@@ -28,9 +28,9 @@
             <DropdownSelector v-model="vari" :options="['{name}', '{age}']" placeholder="变量" />
             <DropdownSelector v-model="oper" :options="['=', '!=', '>', '<', '>=', '<=']" placeholder="条件" />
 
-            <el-input v-model="val" style="width: 30px;" size="small"></el-input>
+            <el-input v-model="val" style="width: 30px;" size="small" placeholder="值"></el-input>
             <span>时，使用翻译</span>
-            <el-input v-model="tr" style="flex: 1;" size="small"></el-input>
+            <el-input v-model="tr" style="flex: 1;" size="small" placeholder="翻译"></el-input>
             <el-button size="small" :icon="Delete" circle title="删除" />
         </div>
     </div>
