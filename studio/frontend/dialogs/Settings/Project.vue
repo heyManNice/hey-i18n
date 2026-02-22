ce6637e48cebb9199d15396e3ffe0c6d13c86d63
heyManNice <3031978990@qq.com>
2026-02-23 01:14:01 +0800

    feat：project添加option

--- studio/frontend/dialogs/Settings/Project.vue
+++ studio/frontend/dialogs/Settings/Project.vue
@@ -6,7 +6,9 @@
             </el-select>
         </el-form-item>
         <el-form-item label="用户初始语言" title="用户首次进入时显示的语言，可设置为自动检测或固定语言">
-            <el-select model-value="自动">
+            <el-select v-model="userInitialLocale">
+                <el-option label="跟随系统" value="system" />
+                <el-option v-for="local in availableLocales" :key="local" :label="local" :value="local" />
             </el-select>
         </el-form-item>
     </el-form>
@@ -21,13 +23,30 @@ import {
 } from 'element-plus';
 
 import {
-    ref
+    ref,
+    computed
 } from 'vue';
 
 import {
     languages
 } from '../../consts/languages';
 
+import mExplorer from '../../models/Explorer';
+
+// 可用的语言
+const availableLocales = computed(() => {
+    const locales = new Set<string>();
+    for (const file of mExplorer.mI18nFiles) {
+        locales.add(file.split('.')[0]);
+    }
+    // 再添加一个原文
+    locales.add(mExplorer.mSourceLocale);
+    return Array.from(locales);
+});
+
 // 项目源语言选项
 const sourceLocale = ref('zh-CN');
+
+// 用户初始语言选项
+const userInitialLocale = ref('system');
 </script>
