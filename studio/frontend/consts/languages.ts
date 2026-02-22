590bd4bd83dc452e21edbb3a819cd351980f904b
heyManNice <3031978990@qq.com>
2026-02-23 01:00:42 +0800

    style

--- studio/frontend/consts/languages.ts
+++ studio/frontend/consts/languages.ts
@@ -1,4 +1,5 @@
 // 同仓库下的 hey-i18n 包含了语言相关的信息。
 import { localeNames } from "../../../src/hey-i18n/languages"
+export { localeNames };
 
 export const languages = Object.keys(localeNames) as Array<keyof typeof localeNames>;
--- studio/frontend/dialogs/Settings/Project.vue
+++ studio/frontend/dialogs/Settings/Project.vue
@@ -1,7 +1,8 @@
 <template>
     <el-form label-width="auto">
         <el-form-item label="项目源语言" title="开发者在代码中编写的原始字符串语言">
-            <el-select model-value="zh-CN">
+            <el-select v-model="sourceLocale">
+                <el-option v-for="local in languages" :key="local" :label="local" :value="local" />
             </el-select>
         </el-form-item>
         <el-form-item label="用户初始语言" title="用户首次进入时显示的语言，可设置为自动检测或固定语言">
@@ -18,4 +19,15 @@ import {
     ElSelect,
     ElOption
 } from 'element-plus';
+
+import {
+    ref
+} from 'vue';
+
+import {
+    languages
+} from '../../consts/languages';
+
+// 项目源语言选项
+const sourceLocale = ref('zh-CN');
 </script>
